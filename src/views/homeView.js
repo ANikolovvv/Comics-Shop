6edee0227d43ba2d01de146216779b7cd5ab5211
Anton Nikolov <92668017+ANikolovvv@users.noreply.github.com>
2022-04-04 16:13:10 +0300

    Create homeView.js

---
+++ src/views/homeView.js
@@ -0,0 +1,7 @@
+import { html} from "../lib.js";
+
+export const templateHome = () => html` <section>
+  <h1>Top 5 Comic Book Sales - December 2022!</h1>
+
+  <img class="home" src="" id="image" />
+</section>`;
