b2f10c333edde3784ef5408f6cb3eda0d7122877
Anton Nikolov <92668017+ANikolovvv@users.noreply.github.com>
2022-04-09 17:53:27 +0300

    Update loginView.js

--- src/views/loginView.js
+++ src/views/loginView.js
@@ -1,1 +1,33 @@
 import { html} from "../lib.js";
+
+
+ export const templateLogin = (onSubmit) => html`<section id="loginPage">
+  <form @submit=${onSubmit}>
+    <fieldset>
+      <legend>Login</legend>
+      <label for="email" class="vhide">Email</label>
+      <input
+        id="email"
+        class="email"
+        name="email"
+        type="text"
+        placeholder="Email"
+      />
+      <label for="password" class="vhide">Password</label>
+      <input
+        id="password"
+        class="password"
+        name="password"
+        type="password"
+        placeholder="Password"
+      />
+      <button type="submit" class="login">Login</button>
+      <p class="field">
+        <span
+          >If you don't have profile click <a class="click" href="/register">here</a></span
+        >
+      </p>
+    </fieldset>
+  </form>
+</section>`;
+
